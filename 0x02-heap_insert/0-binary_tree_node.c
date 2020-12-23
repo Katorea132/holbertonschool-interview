b9b7809161fa3f9ee0c72d9480b1ee8956b1cefb
Katorea <katorea132@gmail.com>
2020-12-22 22:34:59 -0500

    Adding the readme and first file

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - Makes a node node
+ * @parent: parent
+ * @value: value
+ * Return: The new node or Null on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (!node)
+		return (0);
+
+	node->n = value;
+	node->left = 0;
+	node->right = 0;
+    node->parent = 0;
+
+	if (parent)
+		node->parent = parent;
+
+	return (node);
+}
