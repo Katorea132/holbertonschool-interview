688964bf4474c2ae7c45d75ce571244abdb79562
Katorea <katorea132@gmail.com>
2021-01-07 15:15:22 -0500

    Added documentatinn

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-
+"""Documentation, right.
+"""
 
 def minOperations(n):
+    """Calculates minimum amount of operations
+
+    Args:
+        n (int): or not int, who knows
+
+    Returns:
+        int: amount of operations, defaults to 0 if not possible.
+    """
     if n > 1:
         i, tot = 2, 0
         while n / 2 > i:
